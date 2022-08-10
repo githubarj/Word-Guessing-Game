aef4f94bf581ef983f64841fdb09d4f71998d4e8
githubarj <githuba@GitthubaRJ.localdomain>
2022-08-10 18:58:30 +0300

    matching input to word

--- js/index.js
+++ js/index.js
@@ -3,15 +3,15 @@ const resetBtn = document.querySelector(".reset-btn");
 const hint = document.querySelector(".hint span");
 const typingInput = document.querySelector(".typing-input");
 
-
-
-
 // checking if we are on serverside on client side javascript
 // if (typeof window !== "undefined") {
 //   console.log("You are on the browser");
 // } else {
 //   console.log("You are on the server");
 // }
+
+//making word a global variable to access it anywhere
+let word;
 
 resetBtn.addEventListener("click", () => {
   // getting a random object from the wordlist
@@ -20,18 +20,38 @@ resetBtn.addEventListener("click", () => {
   // getting a random word from the object attained above using the key
   // let word = randObj.word;
   // using object destructuring since we are naming it the same as its key
-  let { word } = randObj;
+  word = randObj.word;
 
-  hint.textContent = randObj.hint
+  hint.textContent = randObj.hint;
 
   console.log(word);
 
-  inputs.innerHTML = ``
+  inputs.innerHTML = ``;
   for (letter of word) {
     inputs.innerHTML += `<input type="text" disabled /> `;
   }
-   
-})
+});
 
-document.addEventListener("keydown", () => typingInput.focus)
+function initGame(e) {
+  let key = e.target.value;
+  //lets validate user pressed key is alphabet, character or number
+  if (key.match(/^[A-Za-z]+$/)) {
+    console.log(key);
+    // lets check if the entered key is in the word
+    if (word.includes(key)) {
+      for (let i = 0; i < word.length; i++) {
+        //showing matched letter in the input value
+        if (word[i] === key) {
+          inputs.querySelectorAll("input")[i].value = key;
+        }
+      }
+    } else {
+      console.log("letter not found");
+    }
+  }
+  //reseting the input tagwhen a key is pressed 
+  typingInput.value = "";
+}
 
+typingInput.addEventListener("input", initGame);
+document.addEventListener("keydown", () => typingInput.focus());
